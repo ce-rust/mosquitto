5823aee8280865510856d349ab5130a5b64f92d3
Linus Basig <linus@basig.me>
2020-12-05 13:48:44 +0100

    acknowledge after on_message callback is called

--- lib/handle_publish.c
+++ lib/handle_publish.c
@@ -135,7 +135,6 @@ int handle__publish(struct mosquitto *mosq)
 			return MOSQ_ERR_SUCCESS;
 		case 1:
 			util__decrement_receive_quota(mosq);
-			rc = send__puback(mosq, message->msg.mid, 0);
 			pthread_mutex_lock(&mosq->callback_mutex);
 			if(mosq->on_message){
 				mosq->in_callback = true;
@@ -150,7 +149,7 @@ int handle__publish(struct mosquitto *mosq)
 			pthread_mutex_unlock(&mosq->callback_mutex);
 			message__cleanup(&message);
 			mosquitto_property_free_all(&properties);
-			return rc;
+			return send__puback(mosq, message->msg.mid, 0);
 		case 2:
 			message->properties = properties;
 			util__decrement_receive_quota(mosq);
